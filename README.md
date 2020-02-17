a8eaa2ba8281af40742de4e9c41f2f57b0e03696
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2020-02-17 17:02:17 -0500

    Release/1.0.1 (#1)
    * tries to fix issue with build on github
    
    * 1.0.1
    
    * removes unnecessary packages
    
    * more package work
    
    * removes node-cache dependency
    
    * Fixes test
    
    * exports everything
    
    * tries to get stderr to stdout
    
    * trying fix involving removing .npmrc
    
    * adds step to upload error log
    
    * adds registry and scope to npm
    
    * maybe?
    
    * whoops
    
    * back
    
    * Changes to npm ci
    
    * adds github token

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ![](https://github.com/trapize/web/workflows/Unit%20Tests/badge.svg)
-# Core
+# Web
 Provides an extensible framework design around ease of use to remove much of the boiler plate code for web APIs.
 
 ## Installation
--- spec/web/caching/Cache.Service.spec.ts
+++ spec/web/caching/Cache.Service.spec.ts
@@ -1,15 +1,32 @@
 import 'reflect-metadata';
 import { CacheService } from '../../../src/web/caching/Cache.Service';
 
+const cache = {
+    get: jest.fn(),
+    set: jest.fn()
+};
+
+const factory = {
+    Create: jest.fn().mockReturnValue(cache)
+};
+
+beforeEach(() => {
+    cache.get.mockReset();
+    cache.set.mockReset();
+    factory.Create.mockClear();
+});
+
 describe('Caching Service', () => {
     it('Should cache', async () => {
         const key = 'key';
         const value = 'value';
 
-        const service = new CacheService(<any>{cacheOptions: {stdTTL: 10}});
+        const service = new CacheService(<any>{cacheOptions: {stdTTL: 10}}, factory);
+        cache.get.mockReturnValue(Promise.resolve(undefined));
         const val1 = await service.get(key);
         expect(val1).toBeUndefined();
         service.set(key, value);
+        cache.get.mockReturnValue(Promise.resolve(value));
         const val2 = await service.get(key);
         expect(val2).toBe('value');
     })
